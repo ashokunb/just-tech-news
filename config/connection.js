e951a7bfe963b3fbf40bb6c1b18e9179972738ba
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-05-24 18:06:12 -0500

    formatted files

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,14 @@ const Sequelize = require('sequelize');
 // library; .env stores the name, username and password that gets passed into this file to login w/o using local info.
 require('dotenv').config();
 
-// // create connection to our db
+
 
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 //   host: 'localhost',
 //   dialect: 'mysql',
 //   port: 3306
 // });
-
+// // create connection to our db
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
