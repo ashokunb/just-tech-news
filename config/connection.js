c180112022d13a92ee22d8f5d39e8ae6dc01d361
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-05-24 17:46:02 -0500

    fixed heroku deploy" ;

--- config/connection.js
+++ config/connection.js
@@ -5,14 +5,15 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // // create connection to our db
+
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 //   host: 'localhost',
 //   dialect: 'mysql',
 //   port: 3306
 // });
 
-//lets heroku use jawsbb my sql to run a port on their network and we can 
-//run our own port when we are local
+let sequelize;
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
